7c19032ee87d68e003cf363730a44495ffb1eb72
PpluX <joseluis.hidalgo@gmail.com>
2018-01-22 05:20:43 +0100

    improving Mutex/Spinlock classes

--- px_sched.h
+++ px_sched.h
@@ -376,25 +376,92 @@ namespace px {
   template<class M>
   class Mutex {
   public:
+
+    ~Mutex() { lock(); }
+
     void lock() {
+      std::thread::id tid = std::this_thread::get_id();
+      if (owner_ == tid) {
+        count_++;
+        return;
+      }
       Scheduler::CurrentThreadBeforeLockResource(&mutex_);
       mutex_.lock();
+      owner_ = tid;
+      count_ = 1;
       Scheduler::CurrentThreadAfterLockResource(true);
     }
     void unlock() {
-      Scheduler::CurrentThreadReleasesResource(&mutex_);
-      mutex_.unlock();
+      std::thread::id tid = std::this_thread::get_id();
+      PX_SCHED_CHECK(owner_ == tid, "Invalid Mutex::unlock owner mistmatch");
+      count_--;
+      if (count_ == 0) {
+        owner_ = std::thread::id();
+        Scheduler::CurrentThreadReleasesResource(&mutex_);
+        mutex_.unlock();
+      }
     }
+
     bool try_lock() {
+      std::thread::id tid = std::this_thread::get_id();
+      if (owner_ == tid) {
+        count_++;
+        return true;
+      }
       Scheduler::CurrentThreadBeforeLockResource(&mutex_);
       bool result = mutex_.try_lock();
+      if (result) {
+        owner_ = tid;
+        count_ = 1;
+      }
       Scheduler::CurrentThreadAfterLockResource(result);
       return result;
     }
   private:
+    std::thread::id owner_ ;
+    uint32_t count_ = 0;
     M mutex_;
   };
 
+  //-- Optional: Spinlock ------------------------------------------------------
+  class Spinlock {
+  public:
+    Spinlock() : owner_(std::thread::id()), count_(0) {}
+
+    ~Spinlock() {
+      lock();
+    }
+
+    void lock() {
+      while(!try_lock()) {}
+    }
+
+    void unlock() {
+      std::thread::id tid = std::this_thread::get_id();
+      PX_SCHED_CHECK(owner_ == tid, "Invalid Spinlock::unlock owner mistmatch");
+      count_--;
+      if (count_ == 0) {
+        owner_ = std::thread::id();
+      }
+    }
+
+    bool try_lock() {
+      std::thread::id tid = std::this_thread::get_id();
+      if (owner_ == tid) {
+        count_++;
+        return true;
+      }
+      std::thread::id expected;
+      if (owner_.compare_exchange_weak(expected, tid)) {
+        count_ = 1;
+        return true;
+      }
+      return false;
+    }
+  private:
+    std::atomic<std::thread::id> owner_ ;
+    uint32_t count_;
+  };
 
   //-- Object pool implementation ----------------------------------------------
   template<class T>
